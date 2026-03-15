919cb6d6a0d3ef01341dbea6bd5353746d5204af
makisan <48534295+sandonemaki@users.noreply.github.com>
2026-03-15 15:25:30 +0900

    lint error test

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 const version = "1.0"
 
 // unusedFuncは使われていない
-func unusedFunc() {}
+// func unusedFunc() {}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
